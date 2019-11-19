f20bbde82a5303f563b7fce21f1b1615a73e5def
danielstyrc <daniel.styrc@op.pl>
2019-11-19 16:30:34 +0100

    FrontendTest, login WordPress 20191119 16:30

--- qagui/src/test/java/com/jsystems/qa/qagui/FrontendTest.java
+++ qagui/src/test/java/com/jsystems/qa/qagui/FrontendTest.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import static com.google.common.truth.Truth.assertThat;
+import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FrontendTest extends ConfigFrontend {
 
     @Test
     public void frontTest() {
-
         driver.get("https://www.wordpress.com/");
         WebElement textElement_1 = driver.findElement(By.cssSelector("h1.lpc-headline-title span:nth-child(1)"));
         String text1 = textElement_1.getText();
@@ -27,6 +27,39 @@ public class FrontendTest extends ConfigFrontend {
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
+
+    }
+
+    @Test
+    public void loginTest() {
+        driver.get("https://www.wordpress.com/");
+//        WebElement login = driver.findElement(By.cssSelector(".x-menu-grid.x-menu-grid--logged-out a[title=\"Log In\"]"));
+        WebElement login = driver.findElement(By.xpath("//*[@id=\"lpc-header-nav\"]/div/div/div[1]/header/nav/ul[2]/li[1]/a"));
+        try {
+            sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(login.isDisplayed());
+        login.click();
+
+        try {
+            sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement login2 = driver.findElement(By.id("usernameOrEmail"));
+        WebElement button = driver.findElement(By.xpath("//*[@id=\"primary\"]/div/main/div/div[1]/div/form/div[1]/div[2]/button"));
+
+        try {
+            sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        login2.sendKeys("testautomation112019@wp.pl");
+        button.click();
 
     }
 
